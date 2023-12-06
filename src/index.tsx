c29fd8f980ea2fa64af4ba1746e75c22e2e40056
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-12-06 10:19:04 +0400

    refactor: remove web-vitals and antd

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
-import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Root element not found');
@@ -13,5 +12,3 @@ root.render(
     <App />
   </BrowserRouter>,
 );
-
-reportWebVitals();
