db16a94d96f5ddff3c24b35b03ce0f96cd135bff
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-12-06 12:13:54 +0400

    refactor: change webpack conf for readable classnames in dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,63 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: { main: './src/index.tsx' },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-    publicPath: '',
-  },
-  mode: 'development',
-  devServer: {
-    static: path.resolve(__dirname, './dist'),
-    compress: true,
-    port: 8080,
+module.exports = (env) => {
+  const mode = env.mode || 'development';
+  const isDev = mode === 'development';
 
-    open: true,
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.((sa|sc|c)ss)$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
+  return {
+    entry: { main: './src/index.tsx' },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.js',
+      publicPath: '',
+    },
+    mode: 'development',
+    devServer: {
+      static: path.resolve(__dirname, './dist'),
+      compress: true,
+      port: 8080,
+
+      open: true,
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          use: 'babel-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.((sa|sc|c)ss)$/,
+          use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  auto: (resPath) => resPath.includes('.module.scss'),
+                  localIdentName: isDev ? '[local]--[hash:base64:5]' : '[hash:base64:8]',
+                },
+              },
+            },
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+      new MiniCssExtractPlugin(),
     ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-    new MiniCssExtractPlugin(),
-  ],
+  };
 };
