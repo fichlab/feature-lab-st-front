6fbf0ad110827d12bec6e5bca8530890dc0a00b5
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-12-06 14:03:00 +0400

    feat: add color palette, move common styles files into separate folder

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,3 @@
-import 'normalize.css';
 import { Route, Routes } from 'react-router-dom';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App';
+import './styles/index.scss';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Root element not found');
