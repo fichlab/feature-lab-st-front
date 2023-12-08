3f211da84a8f498e9c192be68b500db95c16b5f7
Pavel Cherdantsev <ch3pash@yandex.ru>
2023-12-08 23:03:15 +0400

    feat: add header draft

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import Footer from '../Footer/Footer';
-import Header from '../Header/Header';
+import { Header } from '../Header/Header';
 import styles from './App.module.scss';
+import Main from '../Main/Main';
 
 const App: React.FC = () => {
   return (
     <div className={styles.page}>
       <Header />
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Main />} />
       </Routes>
       <Footer />
     </div>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,49 @@
-import styles from './Header.module.scss';
+import cl from 'classnames';
+import { Link, NavLink } from 'react-router-dom';
+import Logo from '../../assets/svg/logo.svg';
+import Burger from '../../assets/svg/ri_menu-line.svg';
+import s from './Header.module.scss';
 
-const Header: React.FC = () => {
-  return <header className={styles.header} />;
+export const Header: React.FC = () => {
+  return (
+    <header className={cl(s.header, s.headerWhite)}>
+      <Link to="/" className={s.logo}>
+        <img className={s.logo} src={Logo} alt="Logo" />
+      </Link>
+
+      <nav className={s.nav}>
+        <ul className={cl(s.list)}>
+          <li className={cl(s.listItem)}>
+            <NavLink to="/" className={({ isActive }) => (true ? s.linkActive : '')}>
+              Компетенции
+            </NavLink>
+          </li>
+          <li className={cl(s.listItem)}>
+            <NavLink to="/some-route" className={({ isActive }) => (isActive ? s.linkActive : '')}>
+              Лаборатория
+            </NavLink>
+          </li>
+          <li className={cl(s.listItem)}>
+            <NavLink to="/some-route" className={({ isActive }) => (isActive ? s.linkActive : '')}>
+              Продукты
+            </NavLink>
+          </li>
+          <li className={cl(s.listItem)}>
+            <NavLink to="/some-route" className={({ isActive }) => (isActive ? s.linkActive : '')}>
+              Контакты
+            </NavLink>
+          </li>
+          <li className={cl(s.listItem)}>
+            <NavLink to="/some-route" className={({ isActive }) => (isActive ? s.linkActive : '')}>
+              Личный кабинет
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <div className={s.burger}>
+        <img className={s.burger} src={Burger} alt="Logo" />
+      </div>
+    </header>
+  );
 };
-
-export default Header;
