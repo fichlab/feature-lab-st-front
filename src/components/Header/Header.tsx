1b357e1054c5acf8dfdff40c785f4a2239ff282b
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2023-12-11 13:41:35 +0400

    refactor: center fixed header on realy big screen

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -25,55 +25,67 @@ export const Header: React.FC = () => {
     <header
       // ref={headerRef}
       className={cl(s.header, {
-        [s.headerWhite]: currentScrollY > 1,
-        [s.headerHidden]: scrollDirection === 'down',
+        [s.header_hidden]: scrollDirection === 'down',
       })}>
-      <Link to="/" className={s.logo}>
-        <img className={s.logo} src={Logo} alt="Logo" />
-      </Link>
-      <nav className={cl(s.nav, { [s.navProfile]: false, [s.navMobileOpen]: isNavMobileOpen })}>
-        <button type="button" onClick={handleBurgerClick} className={cl(s.btnMobileMenuClose)} />
+      <div
+        className={cl(s.content, {
+          [s.content_bg_white]: currentScrollY > 1,
+        })}>
+        <Link to="/" className={s.logo}>
+          <img className={s.logo} src={Logo} alt="Logo" />
+        </Link>
+        <nav className={cl(s.nav, { [s.navProfile]: false, [s.navMobileOpen]: isNavMobileOpen })}>
+          <button type="button" onClick={handleBurgerClick} className={cl(s.btnMobileMenuClose)} />
 
-        <ul className={cl(s.list)}>
-          <li
-            className={cl(s.listItem, s.listItemSubMenu)}
-            onMouseLeave={() => setCompetitionsVisible(false)}>
-            <button
-              type="button"
-              onClick={handleCompetitionsButtonClick}
-              className={cl(s.btnSubmenuOpen, {
-                [s.linkActive]: isCompetitionsVisible,
-              })}>
-              Компетенции
-            </button>
-            <Competitions isVisible={isCompetitionsVisible} />
-          </li>
-          <li className={cl(s.listItem)}>
-            <NavLink to="/some-route1" className={({ isActive }) => (isActive ? s.linkActive : '')}>
-              Лаборатория
-            </NavLink>
-          </li>
-          <li className={cl(s.listItem)}>
-            <NavLink to="/some-route2" className={({ isActive }) => (isActive ? s.linkActive : '')}>
-              Продукты
-            </NavLink>
-          </li>
-          <li className={cl(s.listItem)}>
-            <NavLink to="/some-route3" className={({ isActive }) => (isActive ? s.linkActive : '')}>
-              Контакты
-            </NavLink>
-          </li>
-          <li className={cl(s.listItem)}>
-            <NavLink to="/some-route4" className={({ isActive }) => (isActive ? s.linkActive : '')}>
-              Личный кабинет
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+          <ul className={cl(s.list)}>
+            <li
+              className={cl(s.listItem, s.listItemSubMenu)}
+              onMouseLeave={() => setCompetitionsVisible(false)}>
+              <button
+                type="button"
+                onClick={handleCompetitionsButtonClick}
+                className={cl(s.btnSubmenuOpen, {
+                  [s.linkActive]: isCompetitionsVisible,
+                })}>
+                Компетенции
+              </button>
+              <Competitions isVisible={isCompetitionsVisible} />
+            </li>
+            <li className={cl(s.listItem)}>
+              <NavLink
+                to="/some-route1"
+                className={({ isActive }) => (isActive ? s.linkActive : '')}>
+                Лаборатория
+              </NavLink>
+            </li>
+            <li className={cl(s.listItem)}>
+              <NavLink
+                to="/some-route2"
+                className={({ isActive }) => (isActive ? s.linkActive : '')}>
+                Продукты
+              </NavLink>
+            </li>
+            <li className={cl(s.listItem)}>
+              <NavLink
+                to="/some-route3"
+                className={({ isActive }) => (isActive ? s.linkActive : '')}>
+                Контакты
+              </NavLink>
+            </li>
+            <li className={cl(s.listItem)}>
+              <NavLink
+                to="/some-route4"
+                className={({ isActive }) => (isActive ? s.linkActive : '')}>
+                Личный кабинет
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
 
-      <button type="button" onClick={handleBurgerClick} className={s.burger}>
-        <img className={s.burger} src={Burger} alt="Open mobile menu" />
-      </button>
+        <button type="button" onClick={handleBurgerClick} className={s.burger}>
+          <img className={s.burger} src={Burger} alt="Open mobile menu" />
+        </button>
+      </div>
     </header>
   );
 };
