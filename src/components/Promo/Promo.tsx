6dcd64f5459384d1cbbf98969648eaad3092f34a
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2023-12-13 00:48:35 +0400

    feat: add component Promo

--- src/components/Promo/Promo.tsx
+++ src/components/Promo/Promo.tsx
@@ -1,4 +1,5 @@
 import cl from 'classnames';
+import { Button } from '../ui/Button/Button';
 import s from './Promo.module.scss';
 
 export interface IPromoProps {
@@ -8,22 +9,17 @@ export interface IPromoProps {
 export const Promo = ({ className = '' }: IPromoProps): JSX.Element => {
   return (
     <section aria-label="Promo" className={cl(s.promo, className)}>
-      <h1 className={s.promo__title}>фичлаб</h1>
-      <div className={s.promo__divider} />
+      <div className={s.bg_blue} />
 
-      <div className={s.promo__bg_blue} />
+      <h1 className={s.title}>фичлаб</h1>
+      <div className={s.divider} />
+
       <div className={s.headlines}>
-        <p className={s.headline}>Разрабатываем IT-проекты любой сложности</p>
+        <p className={s.headline}>Разрабатываем IT&#8209;проекты любой сложности</p>
         <p className={s.headline}>Обучаем специалистов</p>
         <p className={s.headline}>Создаём продукты</p>
       </div>
-      <div className={s.promo__button}>
-        <div className={s.promo__ellipse1} />
-        <div className={s.promo__ellipse2} />
-        <div className={s.promo__text}>
-          <div className={s.promo__div2}>Свяжитесь с нами</div>
-        </div>
-      </div>
+      <Button className={s.promo__btn} theme="promo" text="Свяжитесь с&nbsp;нами" />
     </section>
   );
 };
