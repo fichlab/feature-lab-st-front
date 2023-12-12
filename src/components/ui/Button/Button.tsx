889cec908576894d2f3169883f07625ebe7a5634
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2023-12-12 20:47:54 +0400

    feat: update Button component

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,33 +1,62 @@
 import cl from 'classnames';
 import s from './Button.module.scss';
+import Spinner from './svg/Icon-SpinnerGap.svg?svgr';
 
 export interface IButtonProps {
   className?: string;
   onClick?: () => void;
-  theme?: 'blue' | 'white' | 'default';
+  theme: 'promo' | 'white' | 'blue';
+  text?: string;
+  isLoading?: boolean;
 }
 
 export const Button = ({
   className = '',
   onClick = () => {},
-  theme = 'default',
+  theme,
+  text = '',
+  isLoading = false,
 }: IButtonProps): JSX.Element => {
   return (
     <>
-      {theme === 'default' && (
-        <button onClick={onClick} type="button" className={cl(s.btn, s.default, className)}>
-          <div className={s.default_ellipse_out}>
-            <div className={s.default_ellipse_in}>
-              <p className={s.default_text}>Свяжитесь с нами</p>
+      {theme === 'promo' && (
+        <button onClick={onClick} type="button" className={cl(s.btn, s.promo, className)}>
+          <div className={s.disk_out}>
+            <div className={s.disk_in}>
+              {!isLoading ? (
+                <p className={s.promoText}>{text}</p>
+              ) : (
+                <Spinner className={cl(s.spinner)} />
+              )}
             </div>
           </div>
         </button>
       )}
-      {theme === 'white' && (
-        <div className={cl(s.btn, s.white, className)}>
-          <div className={s.white_ellipse_out}>
-            <button onClick={onClick} type="button" className={s.white_ellipse_in}>
-              <p className={s.white_text}>Отправить </p>
+      {(theme === 'white' || theme === 'blue') && (
+        <div className={cl(s.btn, s.radiant, className)}>
+          <div className={s.gradient}>
+            <button
+              onClick={onClick}
+              type="button"
+              className={cl(s.disk, {
+                [s.disk_white]: theme === 'white',
+                [s.disk_blue]: theme === 'blue',
+              })}>
+              {!isLoading ? (
+                <p
+                  className={cl(s.text, {
+                    [s.text_blue]: theme === 'white',
+                    [s.text_white]: theme === 'blue',
+                  })}>
+                  {text}
+                </p>
+              ) : (
+                <Spinner
+                  className={cl(s.spinner, {
+                    [s.spinner_blue]: theme === 'white',
+                  })}
+                />
+              )}
             </button>
           </div>
         </div>
