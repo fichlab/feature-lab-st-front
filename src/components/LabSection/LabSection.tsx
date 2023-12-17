479102e63334b2b083b30472baf741bf3197472d
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2023-12-17 23:48:53 +0400

    feat: add adaptive styles to LabSection

--- src/components/LabSection/LabSection.tsx
+++ src/components/LabSection/LabSection.tsx
@@ -20,38 +20,40 @@ export const LabSection: FC<ILabSectionProps> = ({ className = '' }) => {
     <section className={cl(s.labSection, className)}>
       <h2 className={s.sectionTitle}>Ed-tech</h2>
 
-      <ul className={s.list}>
-        <li className={cl(s.card, s.cardPosition, s.cardPosition_1)} key={uuidv4()}>
-          <p className={s.cardNumber}>0.1</p>
-          <p className={s.cardText}>Плюсики обучения, объяснение, зачем это вообще людям надо</p>
-        </li>
-        <li className={cl(s.card, s.cardPosition, s.cardPosition_2)} key={uuidv4()}>
-          <p className={s.cardNumber}>0.2</p>
-          <p className={s.cardText}>Плюсики обучения, объяснение, зачем это вообще людям надо</p>
-        </li>
-        <li className={cl(s.card, s.cardPosition, s.cardPosition_3)} key={uuidv4()}>
-          <p className={s.cardNumber}>0.3</p>
-          <p className={s.cardText}>Плюсики обучения, объяснение, зачем это вообще людям надо</p>
-        </li>
-        <li className={cl(s.card, s.cardPosition, s.cardPosition_4)} key={uuidv4()}>
-          <p className={s.cardNumber}>0.4</p>
-          <p className={s.cardText}>Плюсики обучения, объяснение, зачем это вообще людям надо</p>
-        </li>
-        <li className={cl(s.card, s.cardPosition, s.cardPosition_5)} key={uuidv4()}>
-          <p className={s.cardNumber}>0.5</p>
-          <p className={s.cardText}>Плюсики обучения, объяснение, зачем это вообще людям надо</p>
-        </li>
-        <li className={cl(s.card, s.cardPosition, s.cardPosition_6)} key={uuidv4()}>
-          <p className={s.cardNumber}>0.6</p>
-          <p className={s.cardText}>Плюсики обучения, объяснение, зачем это вообще людям надо</p>
-        </li>
-      </ul>
-      <Button
-        theme="white"
-        text="Лаборатория"
-        className={cl(s.labButton)}
-        onClick={handleOnBtnClick}
-      />
+      <div className={s.sectionContent}>
+        <ul className={s.list}>
+          <li className={cl(s.card, s.cardPosition, s.cardPosition_1)} key={uuidv4()}>
+            <p className={s.cardNumber}>0.1</p>
+            <p className={s.cardText}>Плюсики обучения, объяснение, зачем это вообще людям надо</p>
+          </li>
+          <li className={cl(s.card, s.cardPosition, s.cardPosition_2)} key={uuidv4()}>
+            <p className={s.cardNumber}>0.2</p>
+            <p className={s.cardText}>Плюсики обучения, объяснение, зачем это вообще людям надо</p>
+          </li>
+          <li className={cl(s.card, s.cardPosition, s.cardPosition_3)} key={uuidv4()}>
+            <p className={s.cardNumber}>0.3</p>
+            <p className={s.cardText}>Плюсики обучения, объяснение, зачем это вообще людям надо</p>
+          </li>
+          <li className={cl(s.card, s.cardPosition, s.cardPosition_4)} key={uuidv4()}>
+            <p className={s.cardNumber}>0.4</p>
+            <p className={s.cardText}>Плюсики обучения, объяснение, зачем это вообще людям надо</p>
+          </li>
+          <li className={cl(s.card, s.cardPosition, s.cardPosition_5)} key={uuidv4()}>
+            <p className={s.cardNumber}>0.5</p>
+            <p className={s.cardText}>Плюсики обучения, объяснение, зачем это вообще людям надо</p>
+          </li>
+          <li className={cl(s.card, s.cardPosition, s.cardPosition_6)} key={uuidv4()}>
+            <p className={s.cardNumber}>0.6</p>
+            <p className={s.cardText}>Плюсики обучения, объяснение, зачем это вообще людям надо</p>
+          </li>
+        </ul>
+        <Button
+          theme="white"
+          text="Лаборатория"
+          className={cl(s.labButton)}
+          onClick={handleOnBtnClick}
+        />
+      </div>
     </section>
   );
 };
