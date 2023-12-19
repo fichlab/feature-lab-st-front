b413458059cee218d35fb4160b30c1750f6b4036
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2023-12-19 16:48:42 +0400

    feat: add responsive adaptive to ProductsSection

--- src/components/ProductsSection/ProductsSection.tsx
+++ src/components/ProductsSection/ProductsSection.tsx
@@ -3,6 +3,8 @@ import { FC } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import s from './ProductsSection.module.scss';
 
+import { products } from './productsMockData';
+
 export interface IProductsSectionProps {
   className?: string;
 }
@@ -13,21 +15,21 @@ export const ProductsSection: FC<IProductsSectionProps> = ({ className = '' }) =
       <h2 className={s.sectionTitle}>Продуктовая разработка</h2>
 
       <ul className={s.list}>
-        <li className={cl(s.product)} key={uuidv4()}>
-          <div className={s.productHeader}>
-            <p className={s.productTitle}>Docshablon</p>
-            <p className={s.productInfo}>
-              Разработка сервиса c необходимыми формами и шаблонами документов
-            </p>
-          </div>
-          <div className={s.productFooter}>
-            <p>Документооборот (1-2-3 тега)</p>
-            <p className={s.divider}>/</p>
-            <a href="/" className={s.productLink}>
-              ссылка на сайт
-            </a>
-          </div>
-        </li>
+        {products.map((product) => (
+          <li className={cl(s.product)} key={uuidv4()}>
+            <div className={s.productHeader}>
+              <p className={s.productTitle}>{product.title}</p>
+              <p className={s.productInfo}>{product.info}</p>
+            </div>
+            <div className={s.productFooter}>
+              <p>{product.labels}</p>
+              <p className={s.divider}>/</p>
+              <a href="/" className={s.productLink}>
+                ссылка на сайт
+              </a>
+            </div>
+          </li>
+        ))}
       </ul>
     </section>
   );
