4a6d21c0513ff676261e754799d7bb1a56b291ae
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-12-20 11:34:47 +0400

    fix: remove extra 4px space from remaining icons

--- src/components/ui/icons/PlusIcon/PlusIcon.tsx
+++ src/components/ui/icons/PlusIcon/PlusIcon.tsx
@@ -14,49 +14,50 @@ export const PlusIcon: FC<PlusIconProps> = ({ className = '' }) => {
   };
 
   return (
-    <svg
-      className={cl(className && className, s.plusIcon)}
-      onClick={handleClick}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 80 80"
-      width={80}
-      height={80}
-      fill="none">
-      <rect x="1.75" y="1.75" width="76.5" height="76.5" rx="38.25" fill="white" />
-      <rect
-        x="1.75"
-        y="1.75"
-        width="76.5"
-        height="76.5"
-        rx="38.25"
-        stroke="#002D63"
-        strokeWidth="3.5"
-      />
-      {isPressed ? (
-        <>
-          <line
-            x1="20.2374"
-            y1="19.7626"
-            x2="59.1283"
-            y2="58.6534"
-            stroke="#002D63"
-            strokeWidth="3.5"
-          />
-          <line
-            x1="19.7626"
-            y1="58.7626"
-            x2="58.6534"
-            y2="19.8717"
-            stroke="#002D63"
-            strokeWidth="3.5"
-          />
-        </>
-      ) : (
-        <>
-          <line x1="13" y1="39.25" x2="68" y2="39.25" stroke="#002D63" strokeWidth="3.5" />
-          <line x1="39.25" y1="67" x2="39.25" y2="12" stroke="#002D63" strokeWidth="3.5" />
-        </>
-      )}
-    </svg>
+    <div className={cl(className && className, s.plusIcon)}>
+      <svg
+        onClick={handleClick}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 80 80"
+        width={80}
+        height={80}
+        fill="none">
+        <rect x="1.75" y="1.75" width="76.5" height="76.5" rx="38.25" fill="white" />
+        <rect
+          x="1.75"
+          y="1.75"
+          width="76.5"
+          height="76.5"
+          rx="38.25"
+          stroke="#002D63"
+          strokeWidth="3.5"
+        />
+        {isPressed ? (
+          <>
+            <line
+              x1="20.2374"
+              y1="19.7626"
+              x2="59.1283"
+              y2="58.6534"
+              stroke="#002D63"
+              strokeWidth="3.5"
+            />
+            <line
+              x1="19.7626"
+              y1="58.7626"
+              x2="58.6534"
+              y2="19.8717"
+              stroke="#002D63"
+              strokeWidth="3.5"
+            />
+          </>
+        ) : (
+          <>
+            <line x1="13" y1="39.25" x2="68" y2="39.25" stroke="#002D63" strokeWidth="3.5" />
+            <line x1="39.25" y1="67" x2="39.25" y2="12" stroke="#002D63" strokeWidth="3.5" />
+          </>
+        )}
+      </svg>
+    </div>
   );
 };
--- src/components/ui/icons/baseIcons/ArrowFatRightIcon.tsx
+++ src/components/ui/icons/baseIcons/ArrowFatRightIcon.tsx
@@ -5,21 +5,22 @@ import s from './Icon.module.scss';
 
 export const ArrowFatRightIcon: FC<IconProps> = ({ filled = false, className = '' }) => {
   return (
-    <svg
-      className={cl(s.icon, s.icon__768, className && className)}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 32 32"
-      width="32"
-      height="32"
-      fill="none">
-      <path
-        d="M17 4L29 16L17 28V22H6C5.73478 22 5.48043 21.8946 5.29289 21.7071C5.10536 21.5196 5 21.2652 5 21V11C5 10.7348 5.10536 10.4804 5.29289 10.2929C5.48043 10.1054 5.73478 10 6 10H17V4Z"
-        fill={filled ? '#002D63' : ''}
-        stroke="#002D63"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
+    <div className={cl(s.icon, s.icon__768, className && className)}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 32 32"
+        width="32"
+        height="32"
+        fill="none">
+        <path
+          d="M17 4L29 16L17 28V22H6C5.73478 22 5.48043 21.8946 5.29289 21.7071C5.10536 21.5196 5 21.2652 5 21V11C5 10.7348 5.10536 10.4804 5.29289 10.2929C5.48043 10.1054 5.73478 10 6 10H17V4Z"
+          fill={filled ? '#002D63' : ''}
+          stroke="#002D63"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </div>
   );
 };
