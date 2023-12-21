459dd0c403f05abb9b333e20752ef52c113f4f69
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2023-12-21 14:24:14 +0400

    refactor: update adaptive for Title and SubTitle in AboutUs

--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -1,16 +1,14 @@
 import { FC } from 'react';
+import cl from 'classnames';
 import s from './AboutUs.module.scss';
 
 const AboutUs: FC = () => {
   return (
     <>
       <h1 className={s.pageTitle}>О нас</h1>
-      <div className={s.subTitle}>
-        <div className={s.divider} />
-        Наши компетенции
+      <div className={s.subTitleWrapper}>
+        <p className={s.subTitle}>Наши компетенции</p>
       </div>
-
-      <section className={s.pageTitle} />
     </>
   );
 };
