435d36e462b0f967e7f78045c5a433279fca6650
KazakovMaksim <kazakovmaksimi@gmail.com>
2023-12-21 10:44:28 +0400

    feat: add feedback popup

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,13 +5,19 @@ import { Header } from '../Header/Header';
 import Main from '../Main/Main';
 import { PopupContact } from '../PopupContact/PopupContact';
 import { CookiesToastContainer } from '../ui/CookiesToastContainer/CookiesToastContainer';
+import { PopupFeedback } from '../PopupFeedback/PopupFeedback';
 import s from './App.module.scss';
 
 const App: React.FC = () => {
   const [isPopupContactOpen, setPopupContactIsOpen] = React.useState(false);
+  const [isPopupFeedbackOpen, setIsPopupFeedbackOpen] = React.useState(false);
 
   const closePopupContactPopups = () => {
     setPopupContactIsOpen(false);
+  };
+
+  const closePopupFeedbackPopup = () => {
+    setIsPopupFeedbackOpen(false);
   };
 
   return (
@@ -22,8 +28,10 @@ const App: React.FC = () => {
       </Main>
 
       <PopupContact isOpen={isPopupContactOpen} onClose={closePopupContactPopups} />
+
       <Footer />
       <CookiesToastContainer />
+      <PopupFeedback isOpen={isPopupFeedbackOpen} onClose={closePopupFeedbackPopup} />
     </div>
   );
 };
