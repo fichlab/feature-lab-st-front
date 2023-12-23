17307b515638f176d697732bd3044e008184a8dc
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2023-12-23 21:28:10 +0400

    feat: add nested routing in app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { ROUTE_COMPETENCIES, ROUTE_HOME, SUBROUTE_GAMEDEV } from '../../constants/constants';
+import { AboutUs } from '../../pages/AboutUs/AboutUs';
+import { GameDev } from '../../pages/Competencies/GameDev';
 import Home from '../../pages/Home/Home';
 import Footer from '../Footer/Footer';
 import { Header } from '../Header/Header';
 import Main from '../Main/Main';
 import { PopupContact } from '../PopupContact/PopupContact';
-import { CookiesToastContainer } from '../ui/CookiesToastContainer/CookiesToastContainer';
 import { PopupFeedback } from '../PopupFeedback/PopupFeedback';
+import { CookiesToastContainer } from '../ui/CookiesToastContainer/CookiesToastContainer';
 import s from './App.module.scss';
-import { AboutUs } from '../../pages/AboutUs/AboutUs';
 
 const App: React.FC = () => {
   const [isPopupContactOpen, setPopupContactIsOpen] = React.useState(false);
@@ -27,8 +29,11 @@ const App: React.FC = () => {
       <Header />
       <Main>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about-us" element={<AboutUs />} />
+          <Route path={ROUTE_HOME} element={<Home />} />
+          <Route path={ROUTE_COMPETENCIES} element={<AboutUs />}>
+            <Route path={SUBROUTE_GAMEDEV} element={<GameDev />} />
+            <Route path="some-other-route" element={<GameDev />} />
+          </Route>
         </Routes>
       </Main>
 
--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -1,18 +1,25 @@
 import { FC } from 'react';
+import { Outlet, useMatch } from 'react-router-dom';
+import { ROUTE_COMPETENCIES } from '../../constants/constants';
 import s from './AboutUs.module.scss';
 import { BottomGridSection } from './BottomGridSection/BottomGridSection';
 import { TopGridSection } from './TopGridSection/TopGridSection';
 
 export const AboutUs: FC = () => {
-  return (
-    <>
-      <h1 className={s.pageTitle}>О нас</h1>
-      <div className={s.subTitleWrapper}>
-        <p className={s.subTitle}>Наши компетенции</p>
-      </div>
+  const isCompetenciesDefaultPage = useMatch(ROUTE_COMPETENCIES);
 
-      <TopGridSection />
-      <BottomGridSection />
-    </>
-  );
+  if (isCompetenciesDefaultPage) {
+    return (
+      <>
+        <h1 className={s.pageTitle}>О нас</h1>
+        <div className={s.subTitleWrapper}>
+          <p className={s.subTitle}>Наши компетенции</p>
+        </div>
+
+        <TopGridSection />
+        <BottomGridSection />
+      </>
+    );
+  }
+  return <Outlet />;
 };
