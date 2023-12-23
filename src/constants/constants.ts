fddefdb3af01482251661d4608691edb3947f5d3
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2023-12-23 20:19:50 +0400

    feat: add route names constants

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -6,3 +6,9 @@ export const MIN_LENGTH_EMAIL = 6;
 export const MAX_LENGTH_EMAIL = 60;
 export const MIN_LENGTH_PROJECT = 15;
 export const MAX_LENGTH_PROJECT = 500;
+
+export const ROUTE_HOME = '/';
+export const ROUTE_COMPETENCIES = '/competencies';
+
+// /competencies subroutes
+export const ROUTE_GAMEDEV = `${ROUTE_COMPETENCIES}/gamedev`;
