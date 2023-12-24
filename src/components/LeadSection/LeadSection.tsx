b9f8317a5494126803df5d6e9452411107bfdca4
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2023-12-25 01:16:35 +0400

    refactor: update LeadSection component

--- src/components/LeadSection/LeadSection.tsx
+++ src/components/LeadSection/LeadSection.tsx
@@ -1,5 +1,5 @@
-import { FC } from 'react';
 import cl from 'classnames';
+import { FC } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import s from './LeadSection.module.scss';
 
@@ -13,6 +13,7 @@ export const LeadSection: FC<ILeadSectionProps> = ({ className = '' }) => {
   return (
     <section className={cl(s.lead, className)}>
       <p className={cl(s.info)}>{leadSectionMockData.info}</p>
+
       <div className={cl(s.description)}>
         {leadSectionMockData.description.map((item) => (
           <p key={uuidv4()}>{item}</p>
