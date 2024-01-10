f6b126ffe39694719f5944d85c74585d99aae666
KazakovMaksim <kazakovmaksimi@gmail.com>
2024-01-10 14:42:32 +0400

    feat: add ArrowInCircleIcon for team section

---
+++ src/components/ui/icons/ArrowInCircleIcon/ArrowInCircleIcon.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react';
+import cl from 'classnames';
+import s from './ArrowInCircleIcon.module.scss';
+
+interface ArrowInCircleIconProps {
+  className?: string;
+  reverse: boolean;
+}
+
+export const ArrowInCircleIcon: FC<ArrowInCircleIconProps> = ({
+  className = '',
+  reverse = false,
+}) => {
+  return (
+    <div role="presentation" className={cl(s.ArrowInCircleIcon, reverse && s.reverse, className)}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 140 140" fill="none">
+        <rect x="1" y="1" width="138" height="138" rx="69" stroke="#002D63" strokeWidth="2" />
+        <path d="M120 70L100 58.453V81.547L120 70ZM102 68L20 68V72L102 72V68Z" fill="#002D63" />
+      </svg>
+    </div>
+  );
+};
