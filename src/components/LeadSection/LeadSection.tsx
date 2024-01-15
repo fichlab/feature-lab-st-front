12faa0bbc9d6c76012c62086462e7521f0941e35
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2024-01-15 23:32:37 +0400

    refactor: add LeadSectionCard for better readability

--- src/components/LeadSection/LeadSection.tsx
+++ src/components/LeadSection/LeadSection.tsx
@@ -1,9 +1,9 @@
 import cl from 'classnames';
 import { FC } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import s from './LeadSection.module.scss';
-
 import { leadSectionMockData } from '../../_mockData/LeadSectionMockData';
+import s from './LeadSection.module.scss';
+import { LeadSectionCard } from './LeadSectionCard/LeadSectionCard';
 
 export interface ILeadSectionProps {
   className?: string;
@@ -15,20 +15,14 @@ export const LeadSection: FC<ILeadSectionProps> = ({ className = '' }) => {
       <p className={cl(s.info)}>{leadSectionMockData.info}</p>
 
       <div className={cl(s.description)}>
-        {leadSectionMockData.description.map((item) => (
+        {leadSectionMockData.description.split('\n').map((item) => (
           <p key={uuidv4()}>{item}</p>
         ))}
       </div>
 
       <ul className={cl(s.cards)}>
         {leadSectionMockData.cardsData.map((card) => (
-          <li className={cl(s.card)} key={uuidv4()}>
-            <div className={cl(s.cardHeader)}>
-              <p className={cl(s.cardTitleBig)}>{card.titleBig}</p>
-              {card.title && <p className={cl(s.cardTitle)}>{card.title}</p>}
-            </div>
-            <p className={cl(s.cardText)}>{card.text}</p>
-          </li>
+          <LeadSectionCard card={card} key={uuidv4()} />
         ))}
       </ul>
     </section>
