2981cd34eb3389ee23d948ceb2c51886bcc52e70
KazakovMaksim <kazakovmaksimi@gmail.com>
2024-01-24 13:53:50 +0400

    refactor: mock stages data from mock file instead of using props

--- src/components/WorkStages/WorkStages.tsx
+++ src/components/WorkStages/WorkStages.tsx
@@ -2,20 +2,20 @@ import cl from 'classnames';
 import { FC } from 'react';
 import { Text } from '../ui/Text/Text';
 import s from './WorkStages.module.scss';
-import { WorkStageItem, WorkStagesItem } from './WorkStageItem/WorkStageItem';
+import { WorkStageItem } from './WorkStageItem/WorkStageItem';
+import { workStages as workStagesData } from '../../_mockData/workStagesMockData';
 
 export interface WorkStagesProps {
   className?: string;
-  stages: WorkStagesItem[];
 }
 
-export const WorkStages: FC<WorkStagesProps> = ({ stages, className = '' }) => {
+export const WorkStages: FC<WorkStagesProps> = ({ className = '' }) => {
   return (
     <section className={cl(s.stages, className)}>
       <Text view="title-3" className={s.stagesTitle} tag="h3">
         Этапы работы
       </Text>
-      {stages.map((stage, index) => (
+      {workStagesData.map((stage, index) => (
         <WorkStageItem key={stage.title} stage={stage} stageNum={index + 1} />
       ))}
     </section>
