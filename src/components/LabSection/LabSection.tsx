22c78488678d3df8e8c3d45dc0a2385d2995e935
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2024-01-25 19:00:54 +0400

    refactor: update LabSection component

--- src/components/LabSection/LabSection.tsx
+++ src/components/LabSection/LabSection.tsx
@@ -2,6 +2,7 @@ import cl from 'classnames';
 import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
+import { SectionTitle } from '../SectionTitle/SectionTitle';
 import { Button } from '../ui/Button/Button';
 import s from './LabSection.module.scss';
 
@@ -18,33 +19,43 @@ export const LabSection: FC<ILabSectionProps> = ({ className = '' }) => {
 
   return (
     <section className={cl(s.labSection, className)}>
-      <h2 className={s.sectionTitle}>Ed-tech</h2>
+      <SectionTitle text="Ed-tech" />
 
       <div className={s.sectionContent}>
         <ul className={s.list}>
           <li className={cl(s.card, s.cardPosition, s.cardPosition_1)} key={uuidv4()}>
             <p className={s.cardNumber}>0.1</p>
-            <p className={s.cardText}>Плюсики обучения, объяснение, зачем это вообще людям надо</p>
+            <p className={s.cardText}>Площадка для старта твоей успешной карьеры в IT</p>
           </li>
           <li className={cl(s.card, s.cardPosition, s.cardPosition_2)} key={uuidv4()}>
             <p className={s.cardNumber}>0.2</p>
-            <p className={s.cardText}>Плюсики обучения, объяснение, зачем это вообще людям надо</p>
+            <p className={s.cardText}>
+              Активно применишь все знания, полученные на обучающих курсах
+            </p>
           </li>
           <li className={cl(s.card, s.cardPosition, s.cardPosition_3)} key={uuidv4()}>
             <p className={s.cardNumber}>0.3</p>
-            <p className={s.cardText}>Плюсики обучения, объяснение, зачем это вообще людям надо</p>
+            <p className={s.cardText}>
+              Получишь в портфолио готовый проект под ключ, сделанный тобой от нуля до результата
+            </p>
           </li>
           <li className={cl(s.card, s.cardPosition, s.cardPosition_4)} key={uuidv4()}>
             <p className={s.cardNumber}>0.4</p>
-            <p className={s.cardText}>Плюсики обучения, объяснение, зачем это вообще людям надо</p>
+            <p className={s.cardText}>
+              Поможем собрать рабочую команду под твою идею или ты войдешь в существующий проект
+            </p>
           </li>
           <li className={cl(s.card, s.cardPosition, s.cardPosition_5)} key={uuidv4()}>
             <p className={s.cardNumber}>0.5</p>
-            <p className={s.cardText}>Плюсики обучения, объяснение, зачем это вообще людям надо</p>
+            <p className={s.cardText}>
+              Самые крутые проекты подготовим для показа инвесторам, а также поможем их найти
+            </p>
           </li>
           <li className={cl(s.card, s.cardPosition, s.cardPosition_6)} key={uuidv4()}>
             <p className={s.cardNumber}>0.6</p>
-            <p className={s.cardText}>Плюсики обучения, объяснение, зачем это вообще людям надо</p>
+            <p className={s.cardText}>
+              Получишь официальное рекомендательное письмо и рабочий стаж в IT-компании
+            </p>
           </li>
         </ul>
         <Button
