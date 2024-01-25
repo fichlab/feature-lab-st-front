f0e34da8a71e97f1fca687200224c47f24c2a79f
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2024-01-25 21:17:54 +0400

    refactor:  Home component handles section spacing

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,14 +3,15 @@ import { CompetenciesSection } from '../../components/CompetenciesSection/Compet
 import { LabSection } from '../../components/LabSection/LabSection';
 import { ProductsSection } from '../../components/ProductsSection/ProductsSection';
 import { PromoSection } from '../../components/PromoSection/PromoSection';
+import s from './Home.module.scss';
 
 const Home: FC = () => {
   return (
     <>
-      <PromoSection />
-      <CompetenciesSection />
-      <LabSection />
-      <ProductsSection title="Продуктовая разработка" />
+      <PromoSection className={s.promoSection} />
+      <CompetenciesSection className={s.pageSection} />
+      <LabSection className={s.pageSection} />
+      <ProductsSection title="Продуктовая разработка" className={s.pageSection} />
     </>
   );
 };
